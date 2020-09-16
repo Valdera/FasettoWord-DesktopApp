5673c36623d0b36b376a4cf065c9f084daa96852
Fauzan Valdera <f.valdera@yahoo.co.id>
2020-09-16 23:03:43 +0700

    Add project files.

---
+++ Fasetto.Word/AttachedProperties/ButtonAttachedProperties.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Fasetto.Word {
+    /// <summary>
+    /// The IsBusy attached property for anything that wants to flag if the control is busy <see cref="PasswordBox"/>
+    /// </summary>
+    public class IsBusyProperty : BaseAttachedProperty<IsBusyProperty, bool> {
+
+    }
+}
---
+++ Fasetto.Word/ViewModel/Base/IHavePassword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word {
+    /// <summary>
+    /// An Interface for a class that can provide a secure password
+    /// </summary>
+    public interface IHavePassword {
+        /// <summary>
+        /// The secure password
+        /// </summary>
+        SecureString SecurePassword { get;  }
+    }
+}
